53f524aa2bb7c37d7e5dbcf66201b8b10ee66465
Nesrine HAGGUI <haggui@unistra.fr>
2024-05-28 17:31:16 +0200

    Implémentation fcts fichier config / device

--- src/device.c
+++ src/device.c
@@ -1,4 +1,5 @@
 #include "headers/device.h"
+#include "headers/utils.h"
 
 void device_init(Device *device, MACAddress mac_address, DeviceType type)
 {
@@ -31,7 +32,6 @@ void switch_init(Device *device, MACAddress mac_address, uint16_t priority, uint
     device->switch_info.ports = (Port *) malloc( 15 * sizeof(Port*));
     // Allocate memory for the switching table
     device->switch_info.switching_table = (MACAddress *) malloc( 10 * sizeof(MACAddress*));
-
 }
 
 bool device_is_switch(Device *device)
@@ -56,8 +56,8 @@ void print_station(Device *device)
     //  MAC Address : 00:1A:2B:3C:4D:5E
     //  IP Address : 130.194.30.2
     printf("------------------Station %d------------------\n", device->index);
-    printf("MAC Address : %d\n", device->mac_address);
-    printf("IP Address : %d\n", device->station_info.ip_address);
+    printf("MAC Address : %s\n", mac_address_to_string(&device->mac_address));
+    printf("IP Address : %s\n", ip_address_to_string(&device->station_info.ip_address));
 }
 
 void print_switch(Device *device)
@@ -69,7 +69,7 @@ void print_switch(Device *device)
     //  Priority : 100
     //  Number of Ports : 4
     printf("------------------Switch %d--------------------\n", device->index);
-    printf("MAC Address : %d\n", device->mac_address);
+    printf("MAC Address : %s\n", mac_address_to_string(&device->mac_address));
     printf("Priority : %d\n", device->switch_info.priority);
     printf("Number of Ports : %d\n", device->switch_info.num_ports);
 }
@@ -105,15 +105,47 @@ void device_from_config(Device *device, char *info)
      * If the device is a station, the IP address is the third element in the line
      * If the device is a switch, the priority and the number of ports are the third and fourth elements in the line
      */
+
+    char* output;
+    char* delimiter = ";";
+    split(info, *delimiter, (char*)output);
+
+    if((char)output[0] == '1')
+    {
+        station_init(device, mac_address_from_string((char*)output[1]), ip_address_from_string((char*)output[2]));
+    }
+
+    if((char)output[0] == '2')
+    {
+        switch_init(device, mac_address_from_string((char*)output[1]), (uint16_t)output[2], (uint8_t)output[3]);
+    }
 }
 
-char *device_to_config(Device *device)
+char* device_to_config(Device *device)
 {
     // This function should convert the device structure to a string and return it
 
     // EXPECTED OUTPUT :
     // 2;01:45:23:a6:f7:01;8;1024" <-- Switch
     // 1;54:d6:a6:82:c5:01;130.79.80.1 <-- Station
+
+    char out[250];
+
+    if(device->type == STATION)
+    {
+        char* mac_address = mac_address_to_string(&device->mac_address);
+        char* ip_address = ip_address_to_string(&device->station_info.ip_address);
+        sprintf(out, "1;%s;%s", mac_address, ip_address);
+    }
+
+    if(device->type == SWITCH)
+    {
+        char* mac_address = mac_address_to_string(&device->mac_address);
+        sprintf(out, "2;%s;%s;%s", mac_address, (char*)device->switch_info.ports, (char*)device->switch_info.priority);
+    }
+
+    return out;
+
 }
 
 void switch_print_table(Device *device)
